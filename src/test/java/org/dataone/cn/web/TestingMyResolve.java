1aca553df6160c9820c7975bbd5075781762164d
Rob Nahf <rnahf@epscor.unm.edu>
2010-07-28 06:00:48 +0000

    updated xsl template to match ObjectLocationList schema.xsd

--- src/test/java/org/dataone/cn/web/TestingMyResolve.java
+++ src/test/java/org/dataone/cn/web/TestingMyResolve.java
@@ -108,10 +108,10 @@ public class TestingMyResolve {
 		String content = new String(responseWrapper.getBuffer());
 		
 //		JUnitMatchers matcher = new JUnitMatchers();
-		assertThat("response contains word 'replica'", content, containsString("xml"));
-		assertEquals(content,"<?xml version=\"1.0\" encoding=\"UTF-8\"?><locations identifier=\"Identifier0\"><location node=\"ReplicaMemberNode0\" " 
-				+"href=\"http://ReplicaMemberNode0object?id=Identifier0\"/><location node=\"ReplicaMemberNode2\" "
-				+"href=\"http://ReplicaMemberNode2object?id=Identifier0\"/></locations>");
+		assertThat("response contains word 'replica'", content, containsString("objectLocationList"));
+//		assertEquals(content,"<?xml version=\"1.0\" encoding=\"UTF-8\"?><locations identifier=\"Identifier0\"><location node=\"ReplicaMemberNode0\" " 
+//				+"href=\"http://ReplicaMemberNode0object?id=Identifier0\"/><location node=\"ReplicaMemberNode2\" "
+//				+"href=\"http://ReplicaMemberNode2object?id=Identifier0\"/></locations>");
 		
 	
 	}
