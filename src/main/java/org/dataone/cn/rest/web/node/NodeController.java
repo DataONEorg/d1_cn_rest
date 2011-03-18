9399764fdd231715e88937e23ed4a40f62ed3075
Robert Waltz <rwaltz@utk.edu>
2011-03-18 17:02:58 +0000

    #981 Persist Nodelist in Metacat

--- src/main/java/org/dataone/cn/rest/web/node/NodeController.java
+++ src/main/java/org/dataone/cn/rest/web/node/NodeController.java
@@ -2,19 +2,32 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.dataone.cn.rest.web.node;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.dataone.service.cn.CoordinatingNodeRegister;
+import org.dataone.cn.batch.utils.TypeMarshaller;
+import org.dataone.cn.rest.proxy.controller.AbstractWebController;
+import org.dataone.cn.rest.proxy.service.ProxyObjectService;
+import org.dataone.cn.rest.proxy.util.AcceptType;
+import org.dataone.cn.rest.proxy.http.ProxyServletResponseWrapper;
+import org.dataone.service.exceptions.BaseException;
+import org.dataone.service.exceptions.ServiceFailure;
 import org.dataone.service.types.AuthToken;
+import org.dataone.service.types.Identifier;
 import org.dataone.service.types.NodeList;
+import org.dataone.service.types.SystemMetadata;
+import org.jibx.runtime.JiBXException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.ServletContextAware;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -25,27 +38,101 @@ import org.springframework.web.servlet.ModelAndView;
  * @author waltz
  */
 @Controller
-public class NodeController {
-        private static final String GET_NODELIST_PATH = "/node";
-        private static final String GET_NODE_PATH = "/node/";
-	@Autowired
-	@Qualifier("registerService")
-	CoordinatingNodeRegister registerService;
+public class NodeController extends AbstractWebController implements ServletContextAware {
 
+    private static final String GET_NODELIST_PATH = "/node";
+    private static final String GET_NODE_PATH = "/node/";
+    @Autowired
+    @Qualifier("proxyObjectService")
+    ProxyObjectService proxyObjectService;
+    ServletContext servletContext;
+    private String nodeListIdentifier = "registry";
 
-	@RequestMapping(value = GET_NODELIST_PATH, method = RequestMethod.GET, headers="accept=*/*")
-	public ModelAndView getNodeList(HttpServletRequest request, HttpServletResponse response) throws Exception {
-                AuthToken token = new AuthToken();
-                token.setToken("public");
-                NodeList nodeList = registerService.listNodes(token);
+    @RequestMapping(value = {GET_NODELIST_PATH, GET_NODE_PATH}, method = RequestMethod.GET, headers = "accept=*/*")
+    public ModelAndView getNodeList(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        NodeList nodeList = null;
+        SystemMetadata systemMetadata = null;
+        String metaNodeListPath = null;
+        String currentNodeListIdentifier = nodeListIdentifier;
+        AuthToken token = new AuthToken();
+        token.setToken("public");
+        // Flip through all the registry entries to find the most up-to-date!
+        // XXX probably need to do determine a way to cache this so that
+        // we don't always have to query metacat for it,
+        // but to make it efficient, we will have to base a cache upon
+        // some kind of signal of update with metacat, So re-evaluate code
+        // when JMS is implemented
+        do {
+            // we have made at least one pass of the loop if systemMetadata is not null
+            if (systemMetadata != null) {
+                List<Identifier> obsoletedBy = systemMetadata.getObsoletedByList();
+                currentNodeListIdentifier = obsoletedBy.get(obsoletedBy.size() - 1).getValue();
+            }
+            ProxyServletResponseWrapper metaResponse = new ProxyServletResponseWrapper(response);
+            try {
+                proxyObjectService.getSystemMetadata(servletContext, request, metaResponse, currentNodeListIdentifier, AcceptType.XML);
+            } catch (BaseException ex) {
+                throw new ServiceFailure(ex.getDetail_code(), "Proxied from CoordinatingNodeRegisterImpl.listNodes:" + ex.getDescription());
+            } catch (Exception ex) {
+                throw new ServiceFailure("34501", "Proxied from CoordinatingNodeRegisterImpl.listNodes:" + ex.getMessage());
+            }
+            ByteArrayInputStream inputStream;
 
-		return new ModelAndView("xmlMetaViewResolver", "org.dataone.service.types.NodeList", nodeList);
+            inputStream = new ByteArrayInputStream(metaResponse.getData());
 
-	}
-	@RequestMapping(value = GET_NODE_PATH + "**", method = RequestMethod.GET, headers="accept=*/*")
-	public void getNode(HttpServletRequest request, HttpServletResponse response) throws Exception {
+            try {
+                systemMetadata = TypeMarshaller.unmarshalTypeFromStream(SystemMetadata.class, inputStream);
+            } catch (IOException ex) {
+                throw new ServiceFailure("34502", "Proxied from CoordinatingNodeRegisterImpl.listNodes:" + ex.getMessage());
+            } catch (InstantiationException ex) {
+                throw new ServiceFailure("34503", "Proxied from CoordinatingNodeRegisterImpl.listNodes:" + ex.getMessage());
+            } catch (IllegalAccessException ex) {
+                throw new ServiceFailure("34504", "Proxied from CoordinatingNodeRegisterImpl.listNodes:" + ex.getMessage());
+            } catch (JiBXException ex) {
+                throw new ServiceFailure("34505", "Proxied from CoordinatingNodeRegisterImpl.listNodes:" + ex.getMessage());
+            }
+            response.resetBuffer();
+            response.reset();
+        } while (!(systemMetadata.getObsoletedByList().isEmpty()));
+        ProxyServletResponseWrapper objectResponse = new ProxyServletResponseWrapper(response);
+        try {
+            proxyObjectService.get(servletContext, request, objectResponse, systemMetadata.getIdentifier().getValue(), AcceptType.XML);
+        } catch (BaseException ex) {
+            throw new ServiceFailure(ex.getDetail_code(), "Proxied from CoordinatingNodeRegisterImpl.listNodes:" + ex.getDescription());
+        } catch (Exception ex) {
+            throw new ServiceFailure("34501", "Proxied from CoordinatingNodeRegisterImpl.listNodes:" + ex.getMessage());
+        }
+        // XXX Node list will grow, so this may need to be handled in some other fashion in future
+        // maybe wrap the unmarshalTypeFromStream with an output stream call as well?
+        ByteArrayInputStream inputStream;
 
-		throw new Exception("search Not implemented Yet!");
+        inputStream = new ByteArrayInputStream(objectResponse.getData());
 
-	}
+        try {
+            nodeList = TypeMarshaller.unmarshalTypeFromStream(NodeList.class, inputStream);
+        } catch (IOException ex) {
+            throw new ServiceFailure("34502", "Proxied from CoordinatingNodeRegisterImpl.listNodes:" + ex.getMessage());
+        } catch (InstantiationException ex) {
+            throw new ServiceFailure("34503", "Proxied from CoordinatingNodeRegisterImpl.listNodes:" + ex.getMessage());
+        } catch (IllegalAccessException ex) {
+            throw new ServiceFailure("34504", "Proxied from CoordinatingNodeRegisterImpl.listNodes:" + ex.getMessage());
+        } catch (JiBXException ex) {
+            throw new ServiceFailure("34505", "Proxied from CoordinatingNodeRegisterImpl.listNodes:" + ex.getMessage());
+        }
+
+        return new ModelAndView("xmlNodeListViewResolver", "org.dataone.service.types.NodeList", nodeList);
+
+    }
+
+    @RequestMapping(value = GET_NODE_PATH + "**", method = RequestMethod.GET, headers = "accept=*/*")
+    public void getNode(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        throw new Exception("search Not implemented Yet!");
+
+    }
+
+    @Override
+    public void setServletContext(ServletContext sc) {
+        this.servletContext = sc;
+    }
 }
