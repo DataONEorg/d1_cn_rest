6440a79c0cb9be75cca49d843156e91a1aebc830
Robert Waltz <rwaltz@utk.edu>
2012-03-06 00:36:41 +0000

    Bug #2429: knb mn's failing authorization tests.  CertificateManager now throwing InvalidToken from getSession.

--- src/main/java/org/dataone/cn/rest/filter/v1/PortalCertificateFilter.java
+++ src/main/java/org/dataone/cn/rest/filter/v1/PortalCertificateFilter.java
@@ -6,6 +6,7 @@ package org.dataone.cn.rest.filter.v1;
 
 import java.io.IOException;
 import java.security.cert.X509Certificate;
+import java.util.logging.Level;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -13,9 +14,11 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 import org.dataone.client.auth.CertificateManager;
 import org.dataone.portal.PortalCertificateManager;
+import org.dataone.service.exceptions.InvalidToken;
 import org.dataone.service.types.v1.Session;
 
 /**
@@ -40,7 +43,22 @@ public class PortalCertificateFilter implements Filter {
     	logger.debug("Performing certificate filter");
 
     	// check if we have the certificate (session) already
-    	Session session = CertificateManager.getInstance().getSession((HttpServletRequest) request);
+
+    	Session session = null;
+        try {
+            session = CertificateManager.getInstance().getSession((HttpServletRequest) request);
+        } catch (InvalidToken ex) {
+            HttpServletRequest httpRequest = (HttpServletRequest) request;
+            // If everything beyond this filter has been implemented correctly,
+            // then the InvalidToken will be caught by the rest endpoint whose job
+            // it is to validate any certificate it finds on the request.
+            // Sooo... set the session to something bogus for now
+            // because PortalCertificateManager shouldn't know what to do
+            // and because this filter does not know how each rest endpoint
+            // handles InvalidTokens.
+            logger.error("Invalid Token in the PortalCertificateFilter. Passing to endpoint " + httpRequest.getRequestURI() + " for triage");
+            session = new Session();
+        }
     	if (session == null) {
 	    	// try to get the certificate from the portal instead
 	        X509Certificate certificate = null;
