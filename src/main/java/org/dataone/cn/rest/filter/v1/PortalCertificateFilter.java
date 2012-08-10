276d5be12d9abe9e2fb0dc3f79a78c34156af9fc
Sky Roseboom <sroseboo@epscor.unm.edu>
2012-08-10 04:58:47 +0000

    PortalCertificateManager re-factoring for code re-use.  Encapsulating logic for registering certificates from portal manager, for placing portal certificate in http servlet request object.  Used in PortalCertificateFilter, SessionAuthorizationStrategy (solr_extensions) and SolrClientController (mercury-common)

--- src/main/java/org/dataone/cn/rest/filter/v1/PortalCertificateFilter.java
+++ src/main/java/org/dataone/cn/rest/filter/v1/PortalCertificateFilter.java
@@ -23,8 +23,7 @@
 package org.dataone.cn.rest.filter.v1;
 
 import java.io.IOException;
-import java.security.cert.X509Certificate;
-import java.util.logging.Level;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -32,18 +31,15 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 import org.apache.log4j.Logger;
-import org.dataone.client.auth.CertificateManager;
 import org.dataone.portal.PortalCertificateManager;
 import org.dataone.service.exceptions.InvalidToken;
-import org.dataone.service.types.v1.Session;
 
 /**
- * Adds client certificate to the request when:
- *  1. It is not already available
- *  2. It is available from the portal service
- *  
+ * Adds client certificate to the request when: 1. It is not already available
+ * 2. It is available from the portal service
+ * 
  * @author leinfelder
  */
 public class PortalCertificateFilter implements Filter {
@@ -56,39 +52,28 @@ public class PortalCertificateFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain fc) throws IOException, ServletException {
-		
-    	logger.debug("Performing certificate filter");
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain fc)
+            throws IOException, ServletException {
 
-    	// check if we have the certificate (session) already
+        logger.debug("Performing certificate filter");
 
-    	Session session = null;
         try {
-            session = CertificateManager.getInstance().getSession((HttpServletRequest) request);
+            PortalCertificateManager.getInstance().putPortalCertificateOnRequest(
+                    (HttpServletRequest) request);
         } catch (InvalidToken ex) {
             HttpServletRequest httpRequest = (HttpServletRequest) request;
             // If everything beyond this filter has been implemented correctly,
-            // then the InvalidToken will be caught by the rest endpoint whose job
+            // then the InvalidToken will be caught by the rest endpoint whose
+            // job
             // it is to validate any certificate it finds on the request.
-            // But pass it on the PortalCertificateManager in case that component
+            // But pass it on the PortalCertificateManager in case that
+            // component
             // knows what to do
-            logger.error("Invalid Token in the PortalCertificateFilter. Passing to endpoint " + httpRequest.getRequestURI() + " for triage");
+            logger.error("Invalid Token in the PortalCertificateFilter. Passing to endpoint "
+                    + httpRequest.getRequestURI() + " for triage");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    	if (session == null) {
-	    	// try to get the certificate from the portal instead
-	        X509Certificate certificate = null;
-			try {
-				certificate = PortalCertificateManager.getInstance().getCertificate((HttpServletRequest) request);
-				logger.debug("Proxy certificate for the request = " + certificate);
-				if (certificate != null) {
-				    X509Certificate[] x509Certificates = new X509Certificate[] { certificate };
-					request.setAttribute("javax.servlet.request.X509Certificate", x509Certificates);
-					logger.debug("Added proxy certificate to the request");
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}	
-    	}
 
         // continue the request
         fc.doFilter(request, response);
