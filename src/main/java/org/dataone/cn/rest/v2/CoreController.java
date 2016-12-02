4256d89de44ab2284d0a855c051fe0654e4b634c
Rob Nahf <rnahf@epscor.unm.edu>
2016-12-02 19:37:20 +0000

    added debug logging to cn.synchronize method and config for turning off duplicate check.

--- src/main/java/org/dataone/cn/rest/v2/CoreController.java
+++ src/main/java/org/dataone/cn/rest/v2/CoreController.java
@@ -111,6 +111,9 @@ public class CoreController extends AbstractServiceController implements Servlet
 
     SimpleDateFormat pingDateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz");
 
+    /* probably should be set in dataone-cn-rest-service/etc/dataone/cn/dataoneHazelcast.properties */
+    private static boolean SYNC_CHECK_BEFORE_ADDING = 
+            Settings.getConfiguration().getBoolean("dataone.hazelcast.synchronization.checkBeforeAdding", /* default */ true);
     
     @PostConstruct
     public void init() {
@@ -359,10 +362,24 @@ public class CoreController extends AbstractServiceController implements Servlet
     public void synchronize(HttpServletRequest request, HttpServletResponse response) throws ServiceFailure, NotAuthorized {
 
         String progress = null;
+        long start = 0;
+        long t0 = 0;
+        if (logger.isDebugEnabled()) {
+            start = System.currentTimeMillis();
+            t0 = start;
+        }
         try {
             // get the NodeID from the subject via xref to the nodeList
             Session session = PortalCertificateManager.getInstance().getSession(request);
             final Subject clientSubject = session.getSubject();
+            
+         /////////////  PERFORMANCE TIMING ////////////////
+            if (logger.isDebugEnabled()) {
+                long time1 = System.currentTimeMillis();
+                logger.debug(String.format("in CN.synchronize, got Session in %d millis", time1 = start));
+                start = time1;
+            }
+            
             NodeList nodeList = nodeRegistryService.listNodes();
             List<Node> nodes = nodeList.getNodeList();
             Node firstFoundNode = (Node) CollectionUtils.find(nodes,
@@ -377,8 +394,15 @@ public class CoreController extends AbstractServiceController implements Servlet
                 throw new NotAuthorized("4962", "The client certificate does not find an approved Member Node");
             }
             NodeReference nodeId = firstFoundNode.getIdentifier();
-
             progress = "(a) found an approved MN: " + nodeId.getValue();
+            
+         /////////////  PERFORMANCE TIMING ////////////////
+            if (logger.isDebugEnabled()) {
+                long time1 = System.currentTimeMillis();
+                logger.debug(String.format("in CN.synchronize, got registered Node in %d millis", time1 - start));
+                start = time1;
+                
+            }
 
             // get the pid parameter from the request object directly
             String pidString = request.getParameter("pid");
@@ -393,6 +417,15 @@ public class CoreController extends AbstractServiceController implements Servlet
                 throw new ServiceFailure("4691", "Unexpected Error! The CN could not get the SystemMetadata map!");
             }
 
+         /////////////  PERFORMANCE TIMING ////////////////
+            progress = "(c) got Hz sysMeta map: " + pidString;
+            if (logger.isDebugEnabled()) {
+                long time1 = System.currentTimeMillis();
+                logger.debug(String.format("in CN.synchronize, got the Hz sysMeta map in %d millis", time1 - start));
+                start = time1;
+                
+            }
+            
             SystemMetadata sysmeta = smm.get(pid);
             if (sysmeta != null && !sysmeta.getAuthoritativeMemberNode().equals(nodeId)) {
                 String message = String.format(
@@ -401,18 +434,41 @@ public class CoreController extends AbstractServiceController implements Servlet
                 logger.info(message);
                 throw new NotAuthorized("4692", message);
             }
-
-            progress = "(c) got sysMeta from the CN map...";
+            
+         /////////////  PERFORMANCE TIMING ////////////////
+            progress = "(d) got sysMeta from the CN map...";
+            if (logger.isDebugEnabled()) {
+                long time1 = System.currentTimeMillis();
+                logger.debug(String.format("in CN.synchronize, got the systemMetadata from Hz in %d millis", time1 - start));
+                start = time1;
+                
+            }
 
             if (hzSyncObjectQueue == null) {
                 throw new ServiceFailure("4691", "CN misconfiguration - could not reach hzSyncObjectQueue named "
                         + synchronizationObjectQueueName);
             }
-            progress = "(d) got HzSyncObjectQueue: " + synchronizationObjectQueueName;
+            progress = "(e) got HzSyncObjectQueue: " + synchronizationObjectQueueName;
             // check that the item isn't already in the queue
             SyncObject so = new SyncObject(nodeId.getValue(), pid.getValue());
-            if (!hzSyncObjectQueue.contains(so)) {
+            
+            long split = 0;
+            if (!SYNC_CHECK_BEFORE_ADDING || !hzSyncObjectQueue.contains(so)) {
+                split = System.currentTimeMillis();
                 hzSyncObjectQueue.add(so);
+            }
+            
+         /////////////  PERFORMANCE TIMING ////////////////
+            if (logger.isDebugEnabled()) {
+                long time1 = System.currentTimeMillis();
+           
+                if (split == 0) {
+                    // it was a dupe, only record time to find it
+                    logger.debug(String.format("in CN.synchronize, checked sync queue for duplicate in %d millis", time1 - start));
+                } else {
+                    logger.debug(String.format("in CN.synchronize, checked sync queue for duplicate in %d millis", split - start));
+                    logger.debug(String.format("in CN.synchronize, added to queue in %d millis", time1 - split));
+                }
             }
         } catch (ServiceFailure e) {
             e.setDetail_code("4961");
@@ -425,6 +481,8 @@ public class CoreController extends AbstractServiceController implements Servlet
                     + progress + ":: " + e.toString();
             logger.error(message, e);
             throw new ServiceFailure("4961", message);
+        } finally {
+            logger.debug(String.format("in CN.synchronize for total time of %d millis", System.currentTimeMillis() - t0));
         }
     }
 
